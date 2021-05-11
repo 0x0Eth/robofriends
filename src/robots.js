6c5081f42756ad19f8245d1bece85e3fa123a9eb
0x0Eth <emmanuelalabi016@gmail.com>
2021-05-11 03:15:10 +0100

    Created my first robot cards

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Emmanuel Alabi',
+        username: 'Horpsy',
+        email: 'emmanuelalabi016@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Exvin Howell',
+        username: 'Antonotte',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Okoro Adewale',
+        username: 'okoron',
+        email: 'okon.wale@sapa.ng'
+    },
+    {
+        id: 5,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.oskerf@knox.org'
+    },
+    {
+        id: 6,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'chain_mcdormott@dana.io'
+    },
+    {
+        id: 7,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maximo_Mienow',
+        email: 'sherwood@rosamond.me'
+    },
+    {
+        id: 8,
+        name: 'Okechukwu Obinna',
+        username: 'Odogwu',
+        email: 'odogwu.1@alaba.intl'
+    },
+    {
+        id: 9,
+        name: 'Garba Shehu',
+        username: 'sapa_major',
+        email: 'garba.shehu@itaya.ng'
+    },
+    {
+        id: 10,
+        name: 'Ciroma kanra',
+        username: 'Cinema',
+        email: 'ciroma.kanra@testful.com'
+    },
+]
